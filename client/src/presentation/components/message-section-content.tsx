5617546a052136ebb67bb952c7fd9611ba10c777
Asua Hostin <a01054394111@gmail.com>
2019-11-12 16:30:48 +0900

    feat : snug ui layout
    why : 앞으로 진행될 ui작성에 용이함을 위하여 Layout을 작성하였다. 앞으로의 작업은 Layout에 컴포넌트를 덧붙이는 작업을 하면 된다. issue-#7

---
+++ client/src/presentation/components/message-section-content.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+const MessageSectionContentWrapper = styled.section`
+  width: 100%;
+  height: 100%;
+  background-color: white;
+`;
+
+export const MessageSectionContent: React.FC = () => {
+  return <MessageSectionContentWrapper>ss</MessageSectionContentWrapper>;
+};
---
+++ client/src/presentation/components/snug-header.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+const HeaderWrapper = styled.header`
+  min-height: 50px;
+  width: 100%;
+`;
+export const SnugHeader: React.FC = () => {
+  return <HeaderWrapper>Header</HeaderWrapper>;
+};
