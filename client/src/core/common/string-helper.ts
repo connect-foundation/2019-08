c9e180d8297ba4a6ceabd4ce3e556b09139b189e
Seung Bin . Ko <kosb15@naver.com>
2019-11-14 20:37:43 +0900

    feat: 문자열 길이 비교 기능을 지원하는 StringHelper 객체 추가
    why:
    문자열은 string 타입에서 많은 기능을 지원하고 있다.
    문자열 대한 추가적인 기능을 하나의 객체에서 지원하기 위해 Helper 객체를 추가한다.

---
+++ client/src/core/common/string-helper.ts
@@ -0,0 +1,35 @@
+export class StringHelper {
+    private readonly text: string;
+
+    constructor(text: string) {
+        this.text = text;
+
+    }
+
+    static from(origin: StringHelper) {
+        return new StringHelper(origin.text);
+
+    }
+
+    lessThan(expected: number) {
+        return this.text.length < expected;
+
+    }
+
+    moreThan(expected: number) {
+        return this.text.length > expected;
+
+    }
+
+    getValue() {
+        return this.text;
+
+    }
+
+    is(object: object) {
+        const {text} = object as StringHelper;
+        return Object.is(text, this.text);
+
+    }
+
+}
