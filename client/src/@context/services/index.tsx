5d35a8eae5e9f3f5f0ff2d96178677d2a6304bc7
Seung Bin . Ko <kosb15@naver.com>
2019-11-18 16:32:57 +0900

    fix: ChannelService 에 ChannelRepository 의존성 주입하지 않아 참조하지 못하는 이슈
    why:
    - Context 에서 ChannelRepository 객체를 주입하여, 참조하지 못하는 문제를 해결한다.

--- client/src/@context/services/index.tsx
+++ client/src/@context/services/index.tsx
@@ -1,4 +1,10 @@
-import { RepositoryDependencies } from "../repositories/index";
+import {RepositoryDependencies} from "../repositories/index";
+import {ChannelService} from "../../core/service/channel-service.js";
+
 export class ServiceDependencies {
-  constructor(repositories: RepositoryDependencies) {}
+  private channelService: ChannelService;
+
+  constructor(repositories: RepositoryDependencies) {
+    this.channelService = new ChannelService(repositories.chatRoom.channel);
+  }
 }
