54cfea40d401408ec021404448b888cb2a565ccb
Seung Bin . Ko <kosb15@naver.com>
2019-11-18 23:54:02 +0900

    refactor: 사용하지 않는 axios-config 파일 제거
    why:
    - Axios 디폴트 설정을 하는 AxiosConfig 기능을 AxiosWrapper 객체에서 수행하도록 변경한다.

--- client/src/config/axios-config.ts
+++
@@ -1,7 +0,0 @@
-export class AxiosConfig {
-  static initialize(axios: any): void {
-    axios.defaults.baseURL = process.env.SERVER_DOMAIN;
-    axios.defaults.headers.common['Content-Type'] = 'application/json';
-    axios.defaults.headers.common['Accept'] = 'application/json';
-  }
-}
