68301b8f5bdc9680469e0c5d224ee4714f59844a
Seung Bin . Ko <kosb15@naver.com>
2019-11-19 15:44:01 +0900

    chore: eslint 기준에 맞춰 문자열 double quote 로 변경
    why:
    - 문자열 double quote 로 변경하여 공통된 코드 스타일을 유지한다.

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,20 +1,20 @@
 import express from "express";
-import morgan from 'morgan';
-import path from 'path';
-import cookieParser from 'cookie-parser';
+import morgan from "morgan";
+import path from "path";
+import cookieParser from "cookie-parser";
 import "dotenv/config";
 
-import indexRouter from './routes/index';
+import indexRouter from "./routes/index";
 
 const app = express();
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 app.use(cookieParser(process.env.COOKIE_SECRET));
 
-app.use('/', indexRouter);
+app.use("/", indexRouter);
 
 const server = app.listen(3000, () => {
-    console.log('listen port 3000');
+    console.log("listen port 3000");
 });
--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,7 +1,7 @@
 import {Router, Request, Response} from "express";
 
 const router = Router();
-router.get('/', (req: Request, res: Response) => {
+router.get("/", (req: Request, res: Response) => {
 
 });
 
