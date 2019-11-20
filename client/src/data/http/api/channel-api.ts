967103a7b2e69252a6d0196fbe8f398965c311cf
Asua Hostin <a01054394111@gmail.com>
2019-11-20 10:53:51 +0900

    fear : 채널아이디를 통해 채널의 채팅 목록을 가져오는 fetch 구현
    데이터를 엔티티 형식에 맞게 불러온다. issue-#19

--- client/src/data/http/api/channel-api.ts
+++ client/src/data/http/api/channel-api.ts
@@ -1,8 +1,8 @@
-import {Channel} from "core/entity/channel";
-import {AxiosError, AxiosResponse} from "axios";
-import {ResponseEntity} from "./response/ResponseEntity";
-import {StatusCodes} from "./status-codes";
-import {AxiosWrapper} from "./axios-wrapper";
+import { Channel } from "core/entity/channel";
+import { AxiosError, AxiosResponse } from "axios";
+import { ResponseEntity } from "./response/ResponseEntity";
+import { StatusCodes } from "./status-codes";
+import { AxiosWrapper } from "./axios-wrapper";
 
 export class ChannelApi {
   private axios: any;
@@ -12,29 +12,43 @@ export class ChannelApi {
   }
 
   create(channel: Channel): ResponseEntity<Channel> | boolean {
-    return this.axios.post(`/api/channel/${channel.name.getValue()}`, {
-      name: channel.name.getValue(),
-      description: channel.description.getValue(),
-      visibility: channel.visibility
-    }).then((response: AxiosResponse<ResponseEntity<Channel>>) => {
-      if (StatusCodes.isCreated(response.status)) {
-        return response.data;
-      } else {
-        return false;
-      }
-    }).catch((error: AxiosError) => this.handleError(error, `${channel.name.getValue()} 추가 과정에서 예기치 못한 에러가 발생했습니다.`));
+    return this.axios
+      .post(`/api/channel/${channel.name.getValue()}`, {
+        name: channel.name.getValue(),
+        description: channel.description.getValue(),
+        visibility: channel.visibility
+      })
+      .then((response: AxiosResponse<ResponseEntity<Channel>>) => {
+        if (StatusCodes.isCreated(response.status)) {
+          return response.data;
+        } else {
+          return false;
+        }
+      })
+      .catch((error: AxiosError) =>
+        this.handleError(
+          error,
+          `${channel.name.getValue()} 추가 과정에서 예기치 못한 에러가 발생했습니다.`
+        )
+      );
   }
 
   findByName(channelName: string): ResponseEntity<Channel> | boolean {
-    return this.axios.get(`/api/channels/${channelName}`)
-            .then((response: AxiosResponse<ResponseEntity<Channel>>) => {
-              if (StatusCodes.isOk(response.status)) {
-                return response.data;
-              } else {
-                return false;
-              }
-            })
-            .catch((error: AxiosError) => this.handleError(error, `${channelName} 조회 과정에서 예기치 못한 에러가 발생했습니다.`));
+    return this.axios
+      .get(`/api/channels/${channelName}`)
+      .then((response: AxiosResponse<ResponseEntity<Channel>>) => {
+        if (StatusCodes.isOk(response.status)) {
+          return response.data;
+        } else {
+          return false;
+        }
+      })
+      .catch((error: AxiosError) =>
+        this.handleError(
+          error,
+          `${channelName} 조회 과정에서 예기치 못한 에러가 발생했습니다.`
+        )
+      );
   }
 
   handleError(error: AxiosError, message: string): boolean {
--- client/src/data/http/api/post-api.ts
+++ client/src/data/http/api/post-api.ts
@@ -1,12 +1,29 @@
+import { Post } from "core/entity/post";
 import { Channel } from "core/entity/channel";
 import { StatusCodes } from "./status-codes";
-import { AxiosResponse } from "axios";
+import { AxiosResponse, AxiosError } from "axios";
 import { AxiosWrapper } from "./axios-wrapper";
+import { ResponseEntity } from "./response/ResponseEntity";
 
 export class PostApi {
   private axios: any;
 
   constructor(axios: AxiosWrapper) {
     this.axios = axios.getAxios();
+  }
+
+  getList({ id }: Channel) {
+    return this.axios()
+      .get(`/post/${id}`)
+      .then(({ data, status }: AxiosResponse<ResponseEntity<Post[]>>) => {
+        if (StatusCodes.isOk(status)) return data.payload;
+        return false;
+      })
+      .catch((error: AxiosError) => {
+        if (error.response) return false;
+        throw new Error(
+          `포스트 데이터를 가져오는 과정에서 오류가 발생했습니다 :${error.message}`
+        );
+      });
   }
 }
