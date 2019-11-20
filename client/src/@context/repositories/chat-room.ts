a87a26df42142fde7a7b35bff098422704bcd61f
Asua Hostin <a01054394111@gmail.com>
2019-11-20 14:51:25 +0900

    feat : 챗룸 디펜던시에 포스트레포지토리 의존성 추가

--- client/src/@context/repositories/chat-room.ts
+++ client/src/@context/repositories/chat-room.ts
@@ -1,14 +1,22 @@
-import {ChannelRepositoryType} from "core/use-case/channel-repository-type";
-import {ChannelRepository} from "data/repository/channel-repository";
+import { PostRepository } from "./../../data/repository/post-repository";
+import { PostRepositoryType } from "core/use-case/post-repository-type";
+import { HttpProviderDependencies } from "@context/http-providers/http-providers";
+import { ChannelRepositoryType } from "core/use-case/channel-repository-type";
+import { ChannelRepository } from "data/repository/channel-repository";
 
 export class ChatRoomRepositoryDependency {
   private readonly channelRepository: ChannelRepositoryType;
+  private readonly postRepository: PostRepositoryType;
 
-  constructor(api: any) {
-    this.channelRepository = new ChannelRepository(api);
+  constructor(api: HttpProviderDependencies) {
+    this.channelRepository = new ChannelRepository(api.getChannel());
+    this.postRepository = new PostRepository(api.getPost());
   }
 
-  getChannelRepository() {
+  getChannelRepository(): ChannelRepositoryType {
     return this.channelRepository;
+  }
+  getPostRepository(): PostRepositoryType {
+    return this.postRepository;
   }
 }
