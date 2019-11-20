259c7fdc14d6346864fc388942229b7638fd5fbc
Seung Bin . Ko <kosb15@naver.com>
2019-11-20 16:36:46 +0900

    refactor: TypeORM 설정 검증하는 테스트 코드 추가
    why:
    - .env 설정 정보 기반으로 TypeORM 커넥션 수립 확인을 테스트한다.
    - PROFILE 환경변수 설정하지 않은 경우 'local' 정보가 주입되는지 테스트한다.

--- server/test/app.test.ts
+++ server/test/app.test.ts
@@ -1,4 +1,26 @@
-test("basic", () => {
-    const sum = (a: any, b: any) => a + b;
-    expect(sum(1, 2)).toBe(3);
+import "dotenv/config";
+import "reflect-metadata";
+import {createConnection, Connection} from "typeorm";
+
+describe("Test TypeORM SetUp", () => {
+    describe("local 환경에서 DB 커넥션이 수립되야만 한다.", () => {
+        test("test", (done) => {
+            // given
+            process.env.PROFILE = "local";
+
+            // when
+            createConnection()
+                    .then((connection: Connection) => {
+                      // then
+                        expect(connection).not.toBeNull();
+                        expect(connection.isConnected).toBeTruthy();
+                        done();
+
+                    })
+                    .catch((error: Error) => { throw error; });
+
+        });
+
+    });
+
 });
