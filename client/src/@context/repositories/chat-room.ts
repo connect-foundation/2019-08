52ca247f84f416fffc5cc6c1f8fbc2c5d7cddec6
Asua Hostin <a01054394111@gmail.com>
2019-11-20 14:57:16 +0900

    feat : 레포지토리 디펜던시 수정 및 포스틍 디펜던시를 레포지토리 디펜던시에 추가
    why : 의미상 챗룸과 포스팅은 다른의미를 가지고 서로다른 api를 사용하기 때문에 분리하고 레포지토리 디펜전시에 포스팅 디펜던시를 추가하였다.

--- client/src/@context/repositories/chat-room.ts
+++ client/src/@context/repositories/chat-room.ts
@@ -1,22 +1,15 @@
-import { PostRepository } from "./../../data/repository/post-repository";
-import { PostRepositoryType } from "core/use-case/post-repository-type";
-import { HttpProviderDependencies } from "@context/http-providers/http-providers";
+import { ChannelApi } from "data/http/api/channel-api";
 import { ChannelRepositoryType } from "core/use-case/channel-repository-type";
 import { ChannelRepository } from "data/repository/channel-repository";
 
 export class ChatRoomRepositoryDependency {
   private readonly channelRepository: ChannelRepositoryType;
-  private readonly postRepository: PostRepositoryType;
 
-  constructor(api: HttpProviderDependencies) {
-    this.channelRepository = new ChannelRepository(api.getChannel());
-    this.postRepository = new PostRepository(api.getPost());
+  constructor(api: ChannelApi) {
+    this.channelRepository = new ChannelRepository(api);
   }
 
   getChannelRepository(): ChannelRepositoryType {
     return this.channelRepository;
-  }
-  getPostRepository(): PostRepositoryType {
-    return this.postRepository;
   }
 }
