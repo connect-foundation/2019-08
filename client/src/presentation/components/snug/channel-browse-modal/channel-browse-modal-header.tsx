6e80e520353ec8a9e3a427ef87cf68b9f9b1cea5
sangwon <junothyme@gmail.com>
2019-11-21 12:22:51 +0900

    feat : pop up 채널 생성 Modal when clicking 채널 생성하기
    why : 채널 생성하기 버튼을 클릭하면 기존에 존재하는 채널 모달창을
    띄워야 하기 때문에  context api에 접근하여 action을 disptach했다.

--- client/src/presentation/components/snug/channel-browse-modal/channel-browse-modal-header.tsx
+++ client/src/presentation/components/snug/channel-browse-modal/channel-browse-modal-header.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import { useModalToggledDispatch } from "contexts/modal-context";
 import { CustomButton } from "presentation/components/atomic-reusable/custom-button";
 
 const Header = styled.header`
@@ -15,6 +16,19 @@ const Title = styled.section`
 `;
 
 export const ChannelBrowseModalHeader: React.FC = () => {
+  const dispatch = useModalToggledDispatch();
+
+  const clickHandler = (event: React.MouseEvent<HTMLElement, MouseEvent>) => {
+    dispatch &&
+      dispatch({
+        type: "TOGGLE_CHANNEL_BROWSE_MODAL"
+      });
+    dispatch &&
+      dispatch({
+        type: "TOGGLE_CHANNEL_PLUS_MODAL"
+      });
+  };
+
   return (
     <Header>
       <Title>채널 목록</Title>
@@ -25,6 +39,7 @@ export const ChannelBrowseModalHeader: React.FC = () => {
         size={"big"}
         fontWeight={"bold"}
         fontSize={"0.9rem"}
+        onClick={clickHandler}
       />
     </Header>
   );
