86ed9225a6d410911d8be4824847f8d7ffe1ec69
Asua Hostin <a01054394111@gmail.com>
2019-11-22 10:53:14 +0900

    chore : 리뷰받을 코드  분리

---
+++ review/core/use-case/post-repository-type.ts
@@ -0,0 +1,8 @@
+import { Channel } from "core/entity/channel";
+import { Post, Profile } from "core/entity/post";
+
+export interface PostRepositoryType {
+  getList(channel: Channel): Promise<Post[] | boolean>;
+
+  create(profile: Profile, post: Post): Promise<boolean>;
+}
