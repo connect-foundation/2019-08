65a4e924e6ad3e0c9d9f3a8a5c1534a8c879be5a
sangwon <junothyme@gmail.com>
2019-11-23 16:10:03 +0900

    feat : global header changes due to login status
    why : 사용자가 로그인이 되어 있으면 로그인이 되어 있는 상태에 맞는 Header UI를 보여준다.

--- client/src/presentation/components/atomic-reusable/global-header.tsx
+++ client/src/presentation/components/atomic-reusable/global-header.tsx
@@ -1,8 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 
-import { CustomButton } from "./custom-button";
+import Dubu from "assets/dubu.png";
+import Notification from "assets/notification.png";
 
+import { CustomButton } from "./custom-button";
+import { IconBox } from "./icon-box";
+
+// 참고 : https://www.w3schools.com/css/tryit.asp?filename=trycss_dropdown_button
 const Wrapper = styled.header`
   display: flex;
   justify-content: space-between;
@@ -13,22 +18,83 @@ const Wrapper = styled.header`
   box-sizing: border-box;
 `;
 
+const IconBoxWrapper = styled.section`
+  display: flex;
+  justify-content: flex-end;
+  width: 80%;
+`;
+
+const DropDown = styled.section`
+  position: relative;
+  display: inline-block;
+
+  &:hover {
+    display: block;
+    > div {
+      display: block;
+    }
+  }
+`;
+
+const ContentWrapper = styled.div`
+  display: none;
+  position: absolute;
+  transform: translateX(-90%);
+  background-color: #f9f9f9;
+  min-width: 160px;
+  box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
+  z-index: 1;
+  color: black;
+  padding: 12px 16px;
+  text-decoration: none;
+  &:hover {
+    background-color: #f1f1f1;
+    display: block;
+  }
+`;
+
+const Content = styled.a`
+  color: black;
+  padding: 12px 16px;
+  text-decoration: none;
+  display: block;
+  &:hover {
+    background-color: #f1f1f1;
+  }
+`;
+
 const Title = styled.section`
   color: #ffffff;
   font-size: 1.4rem;
 `;
 
 export const GlobalHeader: React.FC = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState("false");
+
   return (
     <Wrapper>
       <Title> Snug </Title>
-      <CustomButton
-        color={"#fda600"}
-        size={"big"}
-        name={"회원가입"}
-        fontColor={"#ffffff"}
-        fontWeight={"bold"}
-      ></CustomButton>
+      {isLoggedIn ? (
+        <IconBoxWrapper>
+          <DropDown>
+            <IconBox imageSrc={Dubu} />
+            <ContentWrapper>
+              <Content href="#">프로필</Content>
+              <Content href="#">Snug 만들기</Content>
+              <Content href="#">로그아웃</Content>
+            </ContentWrapper>
+          </DropDown>
+          <IconBox imageSrc={Notification} />
+        </IconBoxWrapper>
+      ) : (
+        <CustomButton
+          color={"#fda600"}
+          size={"big"}
+          name={"회원가입"}
+          fontColor={"#ffffff"}
+          fontWeight={"bold"}
+        ></CustomButton>
+      )}
     </Wrapper>
   );
 };
