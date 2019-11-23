578a3599aa97c6b92abe47bcb032d860835c6034
Asua Hostin <a01054394111@gmail.com>
2019-11-23 12:24:31 +0900

    feat : 컬러테마 오브젝트 생성
    이 오브젝트의 색상들이 모든 컴포넌트의 색이 된다.

---
+++ client/src/presentation/theme/color-theme.ts
@@ -0,0 +1,10 @@
+export const colorTheme = {
+  sidebar: "#191d21",
+  sidebarFont: "#f0f0f0",
+  sidbarHover: "#7f7f7f",
+  sidebarSelect: "#0094ff",
+  sidebarSelectFont: "#000000",
+  snug: "#ffffff",
+  snugMainFont: "#000000",
+  snugSubFont: "#7f7f7f"
+};
