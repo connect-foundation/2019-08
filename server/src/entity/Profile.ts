8f47366770598b52b10a14f3c3294b2ab6d5bf33
sangwon <junothyme@gmail.com>
2019-11-25 17:51:25 +0900

    add : extend existing entity classes with BaseEntity
    why : Entity 클래스에서 DB에 접근하여 CRUD를 수행하기 위하여,
    BaseEntity를 확장했다.

--- server/src/entity/Profile.ts
+++ server/src/entity/Profile.ts
@@ -1,51 +1,68 @@
 export type UserRoleType = "admin" | "participant";
 
-import {Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn, OneToMany, ManyToMany} from "typeorm";
-import {Room} from "./Room";
-import {Post} from "./Post";
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  OneToMany,
+  ManyToMany,
+  BaseEntity
+} from "typeorm";
+import { Room } from "./Room";
+import { Post } from "./Post";
 
 @Entity()
-export class Profile {
+export class Profile extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @PrimaryGeneratedColumn()
-    id: number;
+  @Column()
+  name: string;
 
-    @Column()
-    name: string;
+  @Column()
+  status: string;
 
-    @Column()
-    status: string;
+  @Column()
+  thumbnail: string;
 
-    @Column()
-    thumbnail: string;
+  @Column()
+  description: string;
 
-    @Column()
-    description: string;
+  @Column({
+    type: "enum",
+    enum: ["admin", "participant"]
+  })
+  role: UserRoleType;
 
-    @Column({
-        type: "enum",
-        enum: ["admin", "participant"]
-    })
-    role: UserRoleType;
+  @CreateDateColumn()
+  createdAt: Date;
 
-    @CreateDateColumn()
-    createdAt: Date;
+  @UpdateDateColumn()
+  updatedAt: Date;
 
-    @UpdateDateColumn()
-    updatedAt: Date;
-    
-    @Column()
-    deletedAt: Date;
+  @Column({ nullable: true })
+  deletedAt: Date;
 
-    // 생성한 room 목록
-    @OneToMany(type => Room, room => room.owner)
-    myRooms: Room[];
+  // 생성한 room 목록
+  @OneToMany(
+    type => Room,
+    room => room.owner
+  )
+  myRooms: Room[];
 
-    // 참여하고 있는 room 목록
-    @ManyToMany(type => Room, room => room.participants)
-    rooms: Room[];
+  // 참여하고 있는 room 목록
+  @ManyToMany(
+    type => Room,
+    room => room.participants
+  )
+  rooms: Room[];
 
-    // 작성한 Post 목록
-    @OneToMany(type => Post, post => post.owner)
-    posts: Post[];
+  // 작성한 Post 목록
+  @OneToMany(
+    type => Post,
+    post => post.owner
+  )
+  posts: Post[];
 }
