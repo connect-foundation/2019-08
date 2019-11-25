8187f86e4c7eef7225b542f598662936080f6bed
sangwon <junothyme@gmail.com>
2019-11-25 16:53:40 +0900

    feat : write creating channel API
    why : front에서 사용자가 채널을 생성하는 요청을 보냈을 때, 그 요청에
    대응하는 API를 만들었다.

--- server/src/controllers/apis/channel-api-controller.ts
+++ server/src/controllers/apis/channel-api-controller.ts
@@ -1,5 +1,6 @@
-import {Channel} from "../../entity/Channel";
-import {Request, Response} from "express";
+import { Channel } from "../../entity/Channel";
+import { Request, Response } from "express";
+import App from "../../app";
 
 /**
  *
@@ -11,20 +12,51 @@ import {Request, Response} from "express";
  * */
 export const find = async (request: Request, response: Response) => {
   const name = request.params.name;
-  const channel = await Channel.findOne({where: {name: name}});
+  const channel = await Channel.findOne({ where: { name: name } });
   if (!!channel) {
-    return response
-            .status(200)
-            .json({
-              message: "ok",
-              payload: channel
-            });
+    return response.status(200).json({
+      message: "ok",
+      payload: channel
+    });
   } else {
-    return response
-            .status(404)
-            .json({
-              message: "not found",
-              payload: {}
-            });
+    return response.status(404).json({
+      message: "not found",
+      payload: {}
+    });
   }
+};
+
+/**
+ *
+ * name 기준으로 channel 생성
+ * 중복된 name로 channel을 생성하지 못한다.
+ *
+ * @param request
+ * @param response
+ *
+ * */
+export const create = async (request: Request, response: Response) => {
+  const name = request.body.name;
+  const description = request.body.description;
+  const privacy = request.body.privacy;
+
+  const isExisting = await Channel.findOne({ where: { name: name } });
+
+  if (!!isExisting) {
+    return response.status(409).json({
+      message: "given channel name already exists",
+      payload: {}
+    });
+  }
+
+  const channel = await Channel.create({
+    name,
+    description,
+    privacy
+  }).save();
+
+  return response.status(201).json({
+    message: "ok",
+    payload: channel
+  });
 };
--- server/src/routes/chnnel-router.ts
+++ server/src/routes/chnnel-router.ts
@@ -1,6 +1,9 @@
-import {Router} from "express";
+import { Router } from "express";
 import * as ChannelApiController from "../controllers/apis/channel-api-controller";
 
 const router = Router();
+
 router.get("/:name", ChannelApiController.find);
+router.post("/:name", ChannelApiController.create);
+
 export default router;
