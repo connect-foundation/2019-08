4634a429387364d6cf997079d604a412fab150b6
Asua Hostin <a01054394111@gmail.com>
2019-11-25 17:20:08 +0900

    feat : 에러를 처리하고 채널 목록을 불러오는 페치함수를 사용하는 레포지토리 함수를 구현
    why : 실제 ui에서 로직을 숨기고 서비스를 통해만 알수 있도록 하기 위해 내부에서 값을 정제하고 반환함

--- client/src/data/repository/channel-repository.ts
+++ client/src/data/repository/channel-repository.ts
@@ -1,6 +1,7 @@
-import {ChannelApi} from '../http/api/channel-api';
-import {Channel} from "../../core/entity/channel";
-import {ChannelRepositoryType} from "../../core/use-case/channel-repository-type";
+import { ResponseEntity } from "./../http/api/response/ResponseEntity";
+import { ChannelApi } from "../http/api/channel-api";
+import { Channel } from "../../core/entity/channel";
+import { ChannelRepositoryType } from "../../core/use-case/channel-repository-type";
 
 export class ChannelRepository implements ChannelRepositoryType {
   private api: ChannelApi;
@@ -22,6 +23,17 @@ export class ChannelRepository implements ChannelRepositoryType {
     try {
       const responseEntity = await this.api.findByName(channelName);
       return !!responseEntity;
+    } catch (error) {
+      return false;
+    }
+  }
+
+  async getChannels(): Promise<Channel[] | boolean> {
+    try {
+      const ResponseEntity = await this.api.getList();
+      if (ResponseEntity)
+        return (<ResponseEntity<Channel[]>>ResponseEntity).payload;
+      return false;
     } catch (error) {
       return false;
     }
