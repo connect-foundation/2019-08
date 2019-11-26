c0f6b4f3a84061bcce6c9b7bb1d8fc464186e8e4
Seung Bin . Ko <kosb15@naver.com>
2019-11-26 16:03:23 +0900

    refactor: 채널 메시지 목록 조회 기능 대한 통합 테스트 추가
    why:
    - 채널 메시지 목록 조회 기능이 올바르게 동작하는 지 검증할 수 있다

--- server/test/integrate/api/channel-controller.test.ts
+++ server/test/integrate/api/channel-controller.test.ts
@@ -1,4 +1,4 @@
-import * as request from "supertest";
+import request from "supertest";
 import {SuperTest, Test} from "supertest";
 import Application from "../../../src/app";
 import {Channel} from "../../../src/entity/Channel";
@@ -23,7 +23,7 @@ describe("/api/channels", () => {
       done();
     });
 
-    test("should return response has identical channel, 200 status code", runInTransaction(async () => {
+    test("올바른 name 로 요청을 보낸 경우, 주어진 Channel 와 동일해야 하며 200 status code 로 응답해야 한다", runInTransaction(async () => {
       // given
       const channel = await Channel.create({
         name: "snug",
@@ -36,11 +36,15 @@ describe("/api/channels", () => {
               .expect("Content-Type", /json/)
               .expect(200)
               .expect({
-                message: "ok", payload: {...channel}
+                message: "ok", payload: {
+                  ...channel,
+                  createdAt: channel.createdAt.toISOString(),
+                  updatedAt: channel.updatedAt.toISOString()
+                }
               });
     }));
 
-    test("should return response has empty object, 404 status code", runInTransaction(async () => {
+    test("존재하지 않는 name 로 요청을 보낸 경우, 주어진 Channel 와 동일해야 하며 200 status code 로 응답해야 한다", runInTransaction(async () => {
       // given
       const channelName = "snug";
       await Channel.delete({
