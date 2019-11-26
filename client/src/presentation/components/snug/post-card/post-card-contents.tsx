01f4ec1f85cbb1d59797b942c6ba3d5d64581fae
Asua Hostin <a01054394111@gmail.com>
2019-11-26 13:37:17 +0900

    refactor : 포스트카드 컴텐츠의 스타일 컴포넌트 들의 이름을 변경
    why: 기존 메세지라는 이름을 가진 컴포넌트의 이름을 에니티의 이름과 비슷하게 Post를 붙여 만들었다.

--- client/src/presentation/components/snug/post-card/post-card-contents.tsx
+++ client/src/presentation/components/snug/post-card/post-card-contents.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 
-const MessageBox = styled.section`
+const PostBox = styled.section`
   padding: 5px;
   width: 100%;
   height: auto;
@@ -9,22 +9,22 @@ const MessageBox = styled.section`
   font-size: 1rem;
 `;
 
-const MessageDetail = styled.section`
+const PostDetail = styled.section`
   width: 100%;
   height: auto;
   padding-top: 5px;
   padding-bottom: 5px;
 `;
 
-const MessageDetailUsername = styled.span`
+const PostDetailWriterName = styled.span`
   font-weight: bold;
 `;
 
-const MessageDetailTimestamp = styled.span`
+const PostDetailTimestamp = styled.span`
   font-size: 0.75rem;
 `;
 
-const MessageContents = styled.span`
+const PostContents = styled.span`
   word-break: break-all;
   padding-top: 5px;
   padding-bottom: 5px;
@@ -42,12 +42,12 @@ export const PostCardContents: React.FC<PropTypes> = ({
   contents: message
 }) => {
   return (
-    <MessageBox>
-      <MessageDetail>
-        <MessageDetailUsername>{userName}</MessageDetailUsername>
-        <MessageDetailTimestamp>{timestamp}</MessageDetailTimestamp>
-      </MessageDetail>
-      <MessageContents>{message}</MessageContents>
-    </MessageBox>
+    <PostBox>
+      <PostDetail>
+        <PostDetailWriterName>{userName}</PostDetailWriterName>
+        <PostDetailTimestamp>{timestamp}</PostDetailTimestamp>
+      </PostDetail>
+      <PostContents>{message}</PostContents>
+    </PostBox>
   );
 };
