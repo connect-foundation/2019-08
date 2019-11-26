8e55b056c29599b11e184b1606959a32bcc3d807
Seung Bin . Ko <kosb15@naver.com>
2019-11-26 15:50:23 +0900

    feat: 메시지 목록 조회 기능 대한 라우터 추가
    why:
    -  Post Router 추가하여 "/api/channels/:id/posts" 경로 대한 메시지 목록 조회 기능을 매핑한다.
    
    (cherry picked from commit 2feecc1b5bd239af5f57c5acf131cd67f6a0e0a2)

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,11 +1,11 @@
 import "dotenv/config";
 import "reflect-metadata";
-import express, { Express } from "express";
+import express, {Express} from "express";
 import morgan from "morgan";
 import cookieParser from "cookie-parser";
-import { Connection, createConnection } from "typeorm";
-import postRouter from "./routes/post/post-router";
-import channelRouter from "./routes/channel/channel-router";
+import {Connection, createConnection} from "typeorm";
+import indexRouter from "./routes/index";
+import channelRouter from "./routes/channel/chnnel-router";
 
 export default class App {
   private static app: Express;
@@ -13,22 +13,21 @@ export default class App {
 
   static async start() {
     return await createConnection()
-      .then(connection => {
-        this.connection = connection;
-        return this.initializeExpress();
-      })
-      .catch(error => console.error("TypeORM Connection Error: ", error));
+            .then((connection) => {
+              this.connection = connection;
+              return this.initializeExpress();
+            })
+            .catch(error => console.error("TypeORM Connection Error: ", error));
   }
 
   private static initializeExpress() {
     this.app = express();
     this.app.set("port", process.env.PORT || 3000);
-    this.app.set("env", process.env.NODE_ENV);
     this.app.use(morgan("dev"));
     this.app.use(express.json());
-    this.app.use(express.urlencoded({ extended: false }));
+    this.app.use(express.urlencoded({extended: false}));
     this.app.use(cookieParser(process.env.COOKIE_SECRET));
-    this.app.use("/api/posts", postRouter);
+    this.app.use("/", indexRouter);
     this.app.use("/api/channels", channelRouter);
     return this.app;
   }
