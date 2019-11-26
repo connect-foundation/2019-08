99c19597249697d15801e5b9e71cf1cee8dbb7aa
Seung Bin . Ko <kosb15@naver.com>
2019-11-26 15:35:40 +0900

    feat: TypeORM 에서 Pagination, Order 옵션을 지원하는 기능 추가
    why:
    - TypeORM Pagination, Order 옵션을 지원하는 기능을 추가하여, 중복된 설정 코드를 제거할 수 있다.

---
+++ server/src/controller/api/common/order.ts
@@ -0,0 +1,105 @@
+/**
+ *
+ * 정렬 조건
+ *
+ * */
+enum SORT {
+  ASC = "ASC",
+  DESC = "DESC"
+}
+
+/**
+ *
+ * Column 대한 SORT 을 정의하는 타입
+ *
+ *
+ * */
+type Entry = {
+  key: string;
+  value: SORT;
+}
+
+/**
+ *
+ * TypeORM 의 order 옵션을 지원하는 객체
+ *
+ * */
+export class Order {
+  private entries: Array<Entry>;
+
+  constructor() {
+    this.entries = new Array<Entry>();
+  }
+
+  /**
+   *
+   * 특정 칼럼에 대한 정렬 조건을 저장
+   *
+   * @param key Column
+   * @param value SORT (ex, ASC/DESC)
+   * @return itself(Order)
+   *
+   * */
+  add(key: string, value: string): Order {
+    if(this.has(key)) {
+      const sort = SORT[value] || SORT.ASC;
+      this.entries.push({key: key, value: sort});
+    }
+
+    return this;
+  }
+
+  /**
+   *
+   * value 값이 존재 여부를 확인
+   *
+   * @param value Column
+   * @return 존재 true, 미존재 false
+   *
+   * */
+  private has(value: string): boolean {
+    return !!value;
+  }
+
+  /**
+   *
+   * 동일한 key 가 base 에 존재하는 지 확인
+   *
+   * @param base
+   * @param key
+   * @return 미존재 true, 존재 false
+   *
+   * */
+  private isNewKey(base: object, key: string): boolean {
+    return !Object.keys(base).includes(key);
+  }
+
+  /**
+   *
+   * accumulator 객체에 entry 를 추가
+   *
+   * @param accumulator
+   * @param entry
+   * @return accumulator
+   *
+   * */
+  private synthesize(accumulator: object, entry: Entry): object {
+    const {key, value} = entry;
+    if(this.isNewKey(accumulator, key)) {
+      accumulator[key] = value;
+    }
+
+    return accumulator;
+  }
+
+  /**
+   *
+   * entries 를 Object 로 파싱하여 TypeORM 옵션을 지원
+   *
+   * @return order 필드를 가진 Object
+   *
+   * */
+  support(): object {
+    return { order: this.entries.reduce(this.synthesize.bind(this), {}) };
+  }
+}
---
+++ server/test/unit/controller/api/common/order.test.ts
@@ -0,0 +1,35 @@
+import {Order} from "../../../../../src/controller/api/common/order";
+
+describe("Test identifier-validator", () => {
+  describe("Test isOutOfRangeChar() method", () => {
+    test("key 가 입력되지 않은 경우, 무시되어야 한다", () => {
+      const order = new Order();
+      expect(order.add("", "DESC").support()).toEqual({order: {}});
+    });
+
+    test("ASC or DESC 가 아닌 값이 value 로 입력된 경우, default 인 ASC 가 설정되어야 한다", () => {
+      const order = new Order();
+      expect(order.add("first", "wrong_value").support()).toEqual({order: {first: "ASC"}});
+    });
+
+    test("ASC 가 value 로 입력된 경우, ASC 가 설정되어야 한다", () => {
+      const order = new Order();
+      expect(order.add("first", "ASC").support()).toEqual({order: {first: "ASC"}});
+    });
+
+    test("DESC 가 value 로 입력된 경우, DESC 가 설정되어야 한다", () => {
+      const order = new Order();
+      expect(order.add("first", "DESC").support()).toEqual({order: {first: "DESC"}});
+    });
+
+    test("동일한 key 대해 다른 value 가 입력된 경우, 첫 번째로 설정된 value 가 설정되어야 한다", () => {
+      const order = new Order();
+      expect(order.add("first", "ASC").add("first", "DESC").support()).toEqual({order: {first: "ASC"}});
+    });
+
+    test("여러 가지의 정렬 조건이 입력된 경우, 모두 결과에 포함되어야 한다", () => {
+      const order = new Order();
+      expect(order.add("first", "DESC").add("second", "ASC").support()).toEqual({order: {first: "DESC", second: "ASC"}});
+    });
+  });
+});
