bb4857ef64e5f762674853dba3f00ebf7f94d99b
Seung Bin . Ko <kosb15@naver.com>
2019-11-26 17:06:31 +0900

    refactor: StringHelper 객체를 Utility 로 변경
    why:
    - AxiosResponse 는 StringHelper 타입을 인식하지 못하기 때문에 string 타입으로 대체한다.
    - StringHelper 객체가 제공하는 기능은 Utility 로 변경한다.

--- client/test/core/common/string-helper.test.ts
+++ client/test/core/common/string-helper.test.ts
@@ -1,121 +1,155 @@
-import {StringHelper} from "core/common/string-helper";
+import {StringHelper} from "core/utility/string-helper";
 
-describe('StringWrapper Class Test', () => {
-    describe('from() Method Test', () => {
-        it('동일한 타입으로 부터 상태값을 복사해서 새로운 객체를 반환해야 성공한다.', () => {
-            // given
-            const text = 'input';
-            const origin = new StringHelper(text);
-
-            // when
-            const target = StringHelper.from(origin);
-
-            // then
-            expect(target).toEqual(origin);
-            expect(target).not.toBe(origin);
-
-        });
-
-
-    });
-
+describe('StringHelp Test', () => {
     describe('lessThan() Method Test', () => {
         it('파라미터 보다 길이가 짧은 경우, true 를 반환해야 한다.', () => {
             // given
             const text = 'input';
-            const origin = new StringHelper(text);
             const length = 10;
 
             // when & then
-            expect(origin.lessThan(length)).toBeTruthy();
-
+            expect(StringHelper.lessThan(text, length)).toBeTruthy();
         });
 
         it('파라미터 보다 길이가 긴 경우, false 를 반환해야 한다.', () => {
             // given
             const text = 'input';
-            const origin = new StringHelper(text);
             const length = 2;
 
             // when & then
-            expect(origin.lessThan(length)).toBeFalsy();
-
+            expect(StringHelper.lessThan(text, length)).toBeFalsy();
         });
 
         it('파라미터 보다 길이가 같은 경우, false 를 반환해야 한다.', () => {
             // given
             const text = 'input';
-            const origin = new StringHelper(text);
             const length = 5;
 
             // when & then
-            expect(origin.lessThan(length)).toBeFalsy();
-
+            expect(StringHelper.lessThan(text, length)).toBeFalsy();
         });
-
     });
 
     describe('moreThan() Method Test', () => {
         it('파라미터 보다 길이가 짧은 경우, false 를 반환해야 한다.', () => {
             // given
             const text = 'input';
-            const origin = new StringHelper(text);
             const length = 10;
 
             // when & then
-            expect(origin.moreThan(length)).toBeFalsy();
-
+            expect(StringHelper.moreThan(text, length)).toBeFalsy();
         });
 
         it('파라미터 보다 길이가 긴 경우, true 를 반환해야 한다.', () => {
             // given
             const text = 'input';
-            const origin = new StringHelper(text);
             const length = 2;
 
             // when & then
-            expect(origin.moreThan(length)).toBeTruthy();
-
+            expect(StringHelper.moreThan(text, length)).toBeTruthy();
         });
 
         it('파라미터 보다 길이가 같은 경우, false 를 반환해야 한다.', () => {
             // given
             const text = 'input';
-            const origin = new StringHelper(text);
             const length = 5;
 
             // when & then
-            expect(origin.moreThan(length)).toBeFalsy();
-
+            expect(StringHelper.moreThan(text, length)).toBeFalsy();
         });
-
     });
 
-    describe('is() Method Test', () => {
-        it('상태값이 같은 경우, true 를 반환해야 한다.', () => {
+    describe('isInner() Method Test', () => {
+        it('lessThan(), moreThan() 메소드 모두 true 리턴할 경우, true 를 반환해야 한다', () => {
             // given
-            const text = new StringHelper('input');
-            const mirror = new StringHelper('input');
+            const text = "temp";
+            const min = 0;
+            const max = 10;
 
-            // when & then
-            expect(text.is(mirror)).toBeTruthy();
-            expect(text).toEqual(mirror);
+            // when
+            const lessThan = jest.fn();
+            lessThan.mockReturnValue(true);
+            StringHelper.lessThan = lessThan;
 
+            const moreThan = jest.fn();
+            moreThan.mockReturnValue(true);
+            StringHelper.moreThan = moreThan;
 
+            const actual = StringHelper.isInner(text, min, max);
+
+            // then
+            expect(lessThan).toHaveBeenCalledWith(text, max);
+            expect(moreThan).toHaveBeenCalledWith(text, min);
+            expect(actual).toBeTruthy();
         });
 
-        it('상태값이 다른 경우, false 를 반환해야 한다.', () => {
+        it('lessThan() 는 false, moreThan() 는 true 반환하는 경우, false 를 반환해야 한다', () => {
             // given
-            const text = new StringHelper('input');
-            const mirror = new StringHelper('difference');
+            const text = "temp";
+            const min = 0;
+            const max = 10;
 
-            // when & then
-            expect(text.is(mirror)).toBeFalsy();
-            expect(text).not.toEqual(mirror);
+            // when
+            const lessThan = jest.fn();
+            lessThan.mockReturnValue(false);
+            StringHelper.lessThan = lessThan;
 
+            const moreThan = jest.fn();
+            moreThan.mockReturnValue(true);
+            StringHelper.moreThan = moreThan;
+
+            const actual = StringHelper.isInner(text, min, max);
+
+            // then
+            expect(lessThan).toHaveBeenCalledWith(text, max);
+            expect(moreThan).toHaveBeenCalledWith(text, min);
+            expect(actual).toBeFalsy();
         });
 
+        it('lessThan() 는 true, moreThan() 는 false 반환하는 경우, false 를 반환해야 한다', () => {
+            // given
+            const text = "temp";
+            const min = 0;
+            const max = 10;
 
+            // when
+            const lessThan = jest.fn();
+            lessThan.mockReturnValue(true);
+            StringHelper.lessThan = lessThan;
+
+            const moreThan = jest.fn();
+            moreThan.mockReturnValue(false);
+            StringHelper.moreThan = moreThan;
+
+            const actual = StringHelper.isInner(text, min, max);
+
+            // then
+            expect(lessThan).not.toHaveBeenCalledWith(text, max);
+            expect(moreThan).toHaveBeenCalledWith(text, min);
+            expect(actual).toBeFalsy();
+        });
+
+        it('lessThan() 는 false, moreThan() 는 false 반환하는 경우, false 를 반환해야 한다', () => {
+            // given
+            const text = "temp";
+            const min = 0;
+            const max = 10;
+
+            // when
+            const lessThan = jest.fn();
+            lessThan.mockReturnValue(false);
+            StringHelper.lessThan = lessThan;
+
+            const moreThan = jest.fn();
+            moreThan.mockReturnValue(false);
+            StringHelper.moreThan = moreThan;
+
+            const actual = StringHelper.isInner(text, min, max);
+
+            // then
+            expect(lessThan).not.toHaveBeenCalledWith(text, max);
+            expect(moreThan).toHaveBeenCalledWith(text, min);
+            expect(actual).toBeFalsy();
+        });
     });
-
 });
