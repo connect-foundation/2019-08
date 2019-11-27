9ebc21766aecc109ec0695ab2d092de5021db3a1
Seung Bin . Ko <kosb15@naver.com>
2019-11-27 13:22:27 +0900

    chore: 테스트 코드 describe name 을 정확한 target 명으로 변경하여 테스트 의미를 올바르게 부여
    (cherry picked from commit 5d849912eda051a8ca312972ea7e6e294fcc527a)

--- server/test/unit/controller/post-controller.test.ts
+++ server/test/unit/controller/post-controller.test.ts
@@ -7,7 +7,7 @@ import {FOUND_CHANNEL} from "../../../src/controller/api/common/error-message";
 // https://stackoverflow.com/a/52334169/8226611 참고하여 static method mocking
 describe("Test post-controller", () => {
   describe("Test findByChannelId() method", () => {
-    test("Room.findByChannelId() 메소드가 올바른 Post 리스트 반환하는 경우, 응답 데이터에 포함되어야 한다", async () => {
+    test("Channel.findByChannelId() 메소드가 올바른 Post 리스트 반환하는 경우, 응답 데이터에 포함되어야 한다", async () => {
       // given
       const channelId = "123";
       const posts = [{id: 1, contents: "contents_test", imgSrc: "http://localhost"}];
