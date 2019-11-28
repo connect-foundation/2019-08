0426941b746862dbcf2364e22a62048fc2edf940
Seung Bin . Ko <kosb15@naver.com>
2019-11-28 10:59:32 +0900

    refactor: Controller 객체에 status-code, response-form 객체 사용하도록 변경

--- server/src/controller/api/channel-controller.ts
+++ server/src/controller/api/channel-controller.ts
@@ -1,5 +1,7 @@
 import {Room} from "../../entity/Room";
 import {Request, Response} from "express";
+import ResponseForm from "../../utils/response-form";
+import {CONFLICT, CREATED, NOT_FOUND} from "./common/status-code";
 
 /**
  *
@@ -13,15 +15,9 @@ export const find = async (request: Request, response: Response) => {
   const title = request.params.title;
   const channel = await Room.findByTitle(title);
   if (!!channel) {
-    return response.status(200).json({
-      message: "ok",
-      payload: channel
-    });
+    return response.status(CREATED).json(ResponseForm.of<Room>("ok", channel));
   } else {
-    return response.status(404).json({
-      message: "not found",
-      payload: {}
-    });
+    return response.status(NOT_FOUND).json(ResponseForm.of("not found"));
   }
 };
 
@@ -42,10 +38,7 @@ export const create = async (request: Request, response: Response) => {
   const isExisting = await Room.findByTitle(title);
 
   if (!!isExisting) {
-    return response.status(409).json({
-      message: "given channel title already exists",
-      payload: {}
-    });
+    return response.status(CONFLICT).json(ResponseForm.of("given channel title already exists"));
   }
 
   const channel = await Room.create({
@@ -55,8 +48,5 @@ export const create = async (request: Request, response: Response) => {
     isChannel: true
   }).save();
 
-  return response.status(201).json({
-    message: "ok",
-    payload: channel
-  });
+  return response.status(CREATED).json(ResponseForm.of<Room>("ok", channel));
 };
