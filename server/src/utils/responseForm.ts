e9aa401aaa0581e21a720ee06eaf363cdf55d7b7
Seung Bin . Ko <kosb15@naver.com>
2019-11-28 10:54:54 +0900

    refactor: ResponseForm 객체에서 payload 대한 타입을 컴파일 시점에 확인할 수 있도록 제네릭을 적용
    why:
    - 컴파일 시점에 payload 대한 타입을 알 수 있어, 안정성을 보장할 수 있다

--- server/src/utils/responseForm.ts
+++
@@ -1,9 +0,0 @@
-export default class ResponseForm {
-    message: string;
-    payload: any;
-
-    constructor(message: string, payload?: any) {
-        this.message = message;
-        this.payload = payload || {};
-    }
-}
