3f6adfd2e00aa4d7c315c06121d44ff592c5c25a
Asua Hostin <a01054394111@gmail.com>
2019-12-03 18:06:48 +0900

    feat : 로그인 컨트롤러 라우터 등록
    사용하기 위하여 라우터에 등록함

--- server/src/routes/auth/auth-router.ts
+++ server/src/routes/auth/auth-router.ts
@@ -1,5 +1,7 @@
 import { Router } from "express";
+import * as AuthApiController from "../../controller/api/auth-controller";
 
 const router = Router();
 
+router.post("/login", AuthApiController.login);
 export default router;
