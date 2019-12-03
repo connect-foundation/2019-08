323456025a707c48238f078ab785e147cdc76b10
Asua Hostin <a01054394111@gmail.com>
2019-12-03 18:01:28 +0900

    feat : jwt를 활용한 로그인 컨트롤러 구현
    사용자가 자신의 스터그들을 효율적으로 관리할 수 있도록 로그인을 구현하였다. jwt를 활용하여 토큰 인증 방식을 채용하였다.
    -issue-#95

---
+++ server/src/controller/api/auth-controller.ts
@@ -0,0 +1,30 @@
+import { User } from "./../../entity/User";
+import { Request, Response } from "express";
+import { OK, NOT_FOUND } from "./common/status-code";
+import ResponseForm from "../../utils/response-form";
+import jwt from "jsonwebtoken";
+import * as crypto from "bcryptjs";
+
+type bodyType = {
+  email: string;
+  password: string;
+};
+
+export const login = async (request: Request, response: Response) => {
+  try {
+    const { email, password }: bodyType = request.body;
+    const secret = process.env.SECRET_KEY;
+    const user = await User.findOne({ where: { email: email } });
+    const payload = {
+      email: email
+    };
+    const token = jwt.sign(payload, secret);
+    if (crypto.compareSync(password, user.password))
+      throw new Error("패스워드가 틀렸습니다.");
+    return response
+      .status(OK)
+      .json(ResponseForm.of("토큰입니다.", { jwt: token }));
+  } catch (error) {
+    return response.status(NOT_FOUND).json(ResponseForm.of(error.message));
+  }
+};
