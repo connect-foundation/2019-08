0c1635e7cfbfc1968b0a9ef6edf11a5af91ba5c5
Asua Hostin <a01054394111@gmail.com>
2019-12-04 18:33:56 +0900

    feat : 로그인 상태를 받아오는 기능 ui에 적용함
    why : 사용자가 자신이 로그인을 했는지 안했는지 쉽게 알 수 있게하기 위해서 -issue-#107

--- client/src/presentation/pages/home/index.tsx
+++ client/src/presentation/pages/home/index.tsx
@@ -2,11 +2,14 @@ import React, { useState, useEffect } from "react";
 import { PageLayout } from "presentation/components/atomic-reusable/page-layout";
 import { HomeForm } from "./home-form";
 import { HomeSnug } from "./home-snug";
+import { ApplicationProptype } from "prop-types/application-type";
 
-export const Home: React.FC = () => {
+export const Home: React.FC<ApplicationProptype> = ({ Application }) => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  useEffect(() => {});
+  useEffect(() => {
+    setIsLoggedIn(Application.services.authService.isLogined());
+  });
 
-  return <PageLayout>{isLoggedIn ? <HomeForm /> : <HomeSnug />}</PageLayout>;
+  return <PageLayout>{isLoggedIn ? <HomeSnug /> : <HomeForm />}</PageLayout>;
 };
