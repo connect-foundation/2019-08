71634f0c43a9119b79bbf7b8ac738204689b9b5b
Asua Hostin <a01054394111@gmail.com>
2019-12-04 16:19:57 +0900

    feat : 로그인 페치 구현
    why : 로그인을 실행하기 위해서 -issue-#105 #106

---
+++ client/src/data/http/api/auth-api.ts
@@ -0,0 +1,32 @@
+import { User } from "core/entity/user";
+import { AxiosErrorHandler } from "data/http/api/axiosErrorHandler";
+import { StatusCodes } from "./status-codes";
+import { AxiosResponse, AxiosError, AxiosInstance } from "axios";
+import { AxiosWrapper } from "./axios-wrapper";
+import { ResponseEntity } from "./response/ResponseEntity";
+import { WebToken } from "core/entity/token";
+
+export class AuthApi {
+  private axios: AxiosInstance;
+
+  constructor(axios: AxiosWrapper) {
+    this.axios = axios.getAxios();
+  }
+
+  getList(user: User): Promise<ResponseEntity<WebToken<string>> | boolean> {
+    return this.axios
+      .post("/api/auth/login", user)
+      .then(
+        ({ data, status }: AxiosResponse<ResponseEntity<WebToken<string>>>) => {
+          if (StatusCodes.isOk(status)) return data;
+          throw new Error("로그인에 실패 했습니다.");
+        }
+      )
+      .catch((error: AxiosError) => {
+        return AxiosErrorHandler.handleError(
+          error,
+          `로그인을 하는 과정에서 문제가 발생했습니다. : ${error.message}`
+        );
+      });
+  }
+}
