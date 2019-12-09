6d8c36083277584a49992df19e6a599de52ba4be
Asua Hostin <a01054394111@gmail.com>
2019-12-09 16:47:42 +0900

    feat : 스너그 라우터에 하위 프로필 라우터를 추가함
    why : 시멘틱적인 의미로 스너그 아이디를 사용하기 위해서

---
+++ server/src/routes/snug/profile/index.ts
@@ -0,0 +1,6 @@
+import { Router } from "express";
+
+const router = Router({ mergeParams: true });
+
+router.post("/");
+export default router;
--- server/src/routes/snug/snug-router.ts
+++ server/src/routes/snug/snug-router.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
-import { create, findByUserId } from "../../controller/api/snug-controller"
+import { create, findByUserId } from "../../controller/api/snug-controller";
 import inviteRouter from "./invite/invite-router";
-import {isNumeric} from "../../validator/identifier-validator";
-
-const router = Router({mergeParams: true});
+import { isNumeric } from "../../validator/identifier-validator";
+import profileRouter from "./profile";
+const router = Router({ mergeParams: true });
 
 router.use("/:snugId/invite", inviteRouter);
+
+router.use("/:snugId/profile", profileRouter);
 
 router.get("/", findByUserId);
 
