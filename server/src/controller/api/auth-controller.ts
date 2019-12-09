51b1fb1db5817b681f62a720ed433256e514de05
Asua Hostin <a01054394111@gmail.com>
2019-12-09 19:35:58 +0900

    feat : ParticipateIn 생성하는 api 구현
    why : 사용자가 만들어진 채널에 들어가서 채팅할 수 있도록 -issue:#134

--- server/src/controller/api/auth-controller.ts
+++ server/src/controller/api/auth-controller.ts
@@ -15,8 +15,6 @@ type bodyType = {
 export const login = async (request: Request, response: Response) => {
   try {
     const { email, password }: bodyType = request.body;
-    console.log(email, password);
-
     const secret = process.env.SECRET_KEY;
     const user = await User.findOne({ where: { email: email } });
     const payload = {
@@ -25,11 +23,9 @@ export const login = async (request: Request, response: Response) => {
       email: email
     };
     if (!crypto.compareSync(password, user.password)) {
-      console.log("비교 실패");
       throw new Error("패스워드가 틀렸습니다.");
     }
     const token = jwt.sign(payload, secret);
-    console.log("비교성공");
     return response
       .status(OK)
       .json(ResponseForm.of("토큰입니다.", { token: token }));
--- server/src/controller/api/channel-controller.ts
+++ server/src/controller/api/channel-controller.ts
@@ -1,3 +1,6 @@
+import { Profile } from "./../../domain/entity/Profile";
+import { ParticipateIn } from "./../../domain/entity/ParticipateIn";
+import { offerProfileTokenInfo } from "./../../validator/identifier-validator";
 import { Room } from "../../domain/entity/Room";
 import { NextFunction, Request, Response } from "express";
 import ResponseForm from "../../utils/response-form";
@@ -89,4 +92,21 @@ export const create = async (request: Request, response: Response) => {
   return response
     .status(CREATED)
     .json(ResponseForm.of<Room>(CREATE_CHANNEL, channel));
+};
+
+export const join = async (request: Request, response: Response) => {
+  try {
+    const payload: any = <object>offerProfileTokenInfo(request);
+    const { id } = request.body;
+    const room = await Room.findOne({ id: id });
+    const profile = await Profile.findOne({ id: payload.id });
+    const result = await ParticipateIn.create({
+      room: room,
+      participant: profile
+    });
+    if (!result) throw new Error("조인실패");
+    response.status(OK).json(ResponseForm.of("성공", result));
+  } catch (error) {
+    response.status(NOT_FOUND).json(ResponseForm.of(error.message));
+  }
 };
