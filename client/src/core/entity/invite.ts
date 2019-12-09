131cce2f9a6b11a07d77a9546b47f857e5be5662
sangwon <junothyme@gmail.com>
2019-12-09 15:48:33 +0900

    fix: invite entity field를 ?로 수정
    why: 받아도 되지 않는 값들을 안 받아도 동작할 수 있게끔 하기 위하여 ?를
    각 field에 추가했다.

--- client/src/core/entity/invite.ts
+++ client/src/core/entity/invite.ts
@@ -1,7 +1,7 @@
 export interface Invite {
-  id: number;
-  email: string;
-  snug: string;
-  link: string;
-  createAt: Date;
+  id?: number;
+  email?: string;
+  snug?: string;
+  link?: string;
+  createAt?: Date;
 }
