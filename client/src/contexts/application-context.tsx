b4b9ed6348ac6d98adff5ca28f4079abdf3196da
sangwon <junothyme@gmail.com>
2019-12-09 15:36:11 +0900

    feat: context에 application과socket 추가
    why: context에 프로젝트 내부에서 전역적으로 쓰는 application과
    socket을 추가했다. 전과 다른 점은 이 파일들 안에서 provider를 갖춰서
    app.tsx에서 provider를 따로 호출하지 않아도 된다.

--- client/src/contexts/application-context.tsx
+++ client/src/contexts/application-context.tsx
@@ -2,3 +2,15 @@ import React, { createContext } from "react";
 import { Application } from "context.instance";
 
 export const globalApplication = createContext(Application);
+
+export const ApplicationContextProvider = ({
+  children
+}: {
+  children: React.ReactNode;
+}) => {
+  return (
+    <globalApplication.Provider value={Application}>
+      {children}
+    </globalApplication.Provider>
+  );
+};
--- client/src/contexts/socket-context.tsx
+++ client/src/contexts/socket-context.tsx
@@ -1,5 +1,18 @@
-import { createContext } from "react";
+import React, { createContext, useContext } from "react";
 import socketIO from "socket.io-client";
 
-const socket = socketIO(process.env.REACT_APP_SOCKET_SERVER_HOST!);
+const socket = socketIO(process.env.REACT_APP_SOCKET_SERVER_HOST! + "/user", {
+  transports: ["websocket"],
+  upgrade: false
+});
 export const globalSocket = createContext(socket);
+
+export const SocketContextProvider = ({
+  children
+}: {
+  children: React.ReactNode;
+}) => {
+  return (
+    <globalSocket.Provider value={socket}>{children}</globalSocket.Provider>
+  );
+};
