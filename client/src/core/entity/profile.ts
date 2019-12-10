08184595903241ff06385b40c39a4671a647ed14
sangwon <junothyme@gmail.com>
2019-12-10 17:09:38 +0900

    feat: Profile Entity 수정
    why: 향후 profile 관련 fetch 작업을 할 때 쓰는 entity이다. 이는 서버에서
    작업하는 profile과 통일시키려는 의도가 있다.

--- client/src/core/entity/profile.ts
+++ client/src/core/entity/profile.ts
@@ -1,6 +1,15 @@
+export enum Role{
+    "admin"= "admin";
+    "participant"="participant";
+}
+
 export interface Profile {
     id?: number;
     thumbnail?: string;
     name?: string;
+    status?: string;
+    role?:Role;
+    description?: string;
+    phoneNumber?: string;
 }
   
