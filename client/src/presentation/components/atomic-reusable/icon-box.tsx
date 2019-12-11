c75605d69bd1881bfad1ca69be02f3a4ef60ef1d
kyungrae <rlarudfo93@gmail.com>
2019-12-11 19:18:06 +0900

    fix: hover기능이 가능하도록 css 변경

--- client/src/presentation/components/atomic-reusable/icon-box.tsx
+++ client/src/presentation/components/atomic-reusable/icon-box.tsx
@@ -26,8 +26,8 @@ const CustomImgBox = styled.section<CustomImgBoxProps>`
   max-width: ${props => (props.size ? props.size : "30px")};
   width: ${props => (props.size ? props.size : "30px")};
   height: ${props => (props.size ? props.size : "30px")};
-  background-color: ${props =>
-    props.backgroundColor ? props.backgroundColor : ""}
+  ${props =>
+    props.backgroundColor ? `background-color: ${props.backgroundColor}` : ""};
   &:hover {
     background-color: #39515a;
   }
