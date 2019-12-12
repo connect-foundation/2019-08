6b583c9bb6d5d36545f174f6a3ae3c43083d0dbc
Asua Hostin <a01054394111@gmail.com>
2019-12-12 16:21:33 +0900

    feat : 기능이 완료 되어 프리뷰 ui가 상시적으로 나타난다. 프로필이 완료가 되면 ui와 기능을 연결한다.
    프리뷰 ui 완료

--- client/src/presentation/pages/snug/message-section/content.tsx
+++ client/src/presentation/pages/snug/message-section/content.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import { ChatContent } from "presentation/components/snug/chat-container";
 import { ChatInputBox } from "presentation/components/snug/chat-input-box";
@@ -6,6 +6,7 @@ import { MessageContextProvider } from "contexts/messages-context";
 import { Preview } from "presentation/components/snug/preview";
 import { AppChannelMatchProps } from "prop-types/match-extends-types";
 import { FileUploadModal } from "presentation/components/snug/file-upload-modal";
+import { usePathParameter } from "contexts/path-parameter-context";
 
 const MessageSectionContentWrapper = styled.section`
   width: 100%;
@@ -17,11 +18,29 @@ const MessageSectionContentWrapper = styled.section`
 `;
 
 export const MessageSectionContent: React.FC<AppChannelMatchProps> = props => {
+  const { Application, history } = props;
   const [isParticipated, setIsParticipated] = useState(false);
+  const pathParameter = usePathParameter();
   // file upload 모달
   // modal state 관리하는 함수 전달
   // file input changed 발생시 modal 활성화
   const [onModal, setModal] = useState(false);
+
+  useEffect(() => {
+    isInParticipating();
+  }, [pathParameter]);
+
+  const isInParticipating = async () => {
+    try {
+      const result = await Application.services.channelService.isInParticipating(
+        pathParameter.channelId!
+      );
+      console.log(result);
+      setIsParticipated(result);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   const openModal = () => {
     setModal(true);
