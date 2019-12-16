8fdc01e89529825fc72ca47f50d5806bd940efe5
sangwon <junothyme@gmail.com>
2019-12-16 18:22:28 +0900

    refactor: 서버 구조 변경
    why: 채팅 이벤트 처리에 대한 책임을 action으로 분리했다.

--- server/src/model/chat/post-info.ts
+++ server/src/model/chat/post-info.ts
@@ -1,27 +1,34 @@
-import {ProfileInfo} from "../profile/profile-info";
+import { ProfileInfo } from "../profile/profile-info";
 import _ from "lodash";
-import {Post} from "../../domain/entity/Post";
+import { Post } from "../../domain/entity/Post";
+import { Room } from "../../domain/entity/Room";
 
 export class PostInfo {
   private readonly id: string;
   private readonly contents: string;
   private readonly imgSrc: string;
   private readonly profile: ProfileInfo;
+  private readonly room: Room;
+  private readonly parent: Post;
   private readonly replyCount: number;
   private readonly updatedAt: Date;
 
   private constructor(
-          id: string,
-          contents: string,
-          imgSrc: string,
-          profile: ProfileInfo,
-          repliesCount: number,
-          updatedAt: Date
+    id: string,
+    contents: string,
+    imgSrc: string,
+    profile: ProfileInfo,
+    room: Room,
+    parent: Post,
+    repliesCount: number,
+    updatedAt: Date
   ) {
     this.id = id;
     this.contents = contents;
     this.imgSrc = imgSrc;
     this.profile = _.cloneDeep(profile);
+    this.room = _.cloneDeep(room);
+    this.parent = _.cloneDeep(parent);
     this.replyCount = repliesCount;
     this.updatedAt = updatedAt;
   }
@@ -31,14 +38,17 @@ export class PostInfo {
   }
 
   public static fromPost(post: Post, replyCount?: number): PostInfo {
-    const {
-      id,
+    const { id, contents, imgSrc, profile, room, parent, updatedAt } = post;
+    const _replyCount = PostInfo.calculateReplyCount(replyCount);
+    return new PostInfo(
+      id.toString(),
       contents,
       imgSrc,
-      profile,
+      ProfileInfo.fromProfile(profile),
+      room,
+      parent,
+      _replyCount,
       updatedAt
-    } = post;
-    const _replyCount = PostInfo.calculateReplyCount(replyCount);
-    return new PostInfo(id.toString(), contents, imgSrc, ProfileInfo.fromProfile(profile), _replyCount, updatedAt);
+    );
   }
 }
--- server/src/socket/action/snug.ts
+++ server/src/socket/action/snug.ts
@@ -1,5 +1,8 @@
-import {Room} from "../../domain/entity/Room";
+import { Room } from "../../domain/entity/Room";
 import SocketIO from "socket.io";
+import { PostInfo } from "../../model/chat/post-info";
+import ResponseForm from "../../utils/response-form";
+import { publishIO } from "../socket-manager";
 
 const takeId = (room: Room): string => room.id.toString();
 
@@ -7,14 +10,15 @@ const takeId = (room: Room): string => room.id.toString();
  * request에 포함된 profile id를 기반으로
  * profileId가 참여하고 있는 Room에 조인한다.
  */
-export const join = async (socket: SocketIO.Socket, profileId: string): Promise<void> => {
+export const join = async (
+  socket: SocketIO.Socket,
+  profileId: string
+): Promise<void> => {
   try {
     const rooms = await Room.find();
-    rooms.map(takeId)
-            .forEach(roomId => {
-              socket.join(roomId);
-
-            });
+    rooms.map(takeId).forEach(roomId => {
+      socket.join(roomId);
+    });
   } catch (error) {
     console.error(error);
   }
@@ -24,14 +28,33 @@ export const join = async (socket: SocketIO.Socket, profileId: string): Promise<
  * request에 포함된 profile id를 기반으로
  * profileId가 참여하고 있는 Room에 나간다.
  */
-export const leave = async (socket: SocketIO.Socket, profileId: string): Promise<void> => {
+export const leave = async (
+  socket: SocketIO.Socket,
+  profileId: string
+): Promise<void> => {
   try {
     const rooms = await Room.find();
-    rooms.map(takeId)
-            .forEach(roomId => {
-              socket.leave(roomId);
-            });
+    rooms.map(takeId).forEach(roomId => {
+      socket.leave(roomId);
+    });
   } catch (error) {
     console.error(error);
+  }
+};
+
+export const sendMessage = (
+  room: string,
+  event: string,
+  message: string,
+  postInfo: PostInfo
+): boolean => {
+  try {
+    return publishIO()
+      .of("/snug")
+      .in(room)
+      .emit(event, ResponseForm.of<PostInfo>(message, postInfo));
+  } catch (error) {
+    console.error(error);
+    return false;
   }
 };
