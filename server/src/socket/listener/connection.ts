5e400df8e1ab755c5955b4ffa35dec594daacebc
kyungrae <rlarudfo93@gmail.com>
2019-12-18 16:56:04 +0900

    fix: 방 참가, 방 생성 했을 때, join evnent emit

--- server/src/socket/listener/connection.ts
+++ server/src/socket/listener/connection.ts
@@ -1,6 +1,6 @@
 import SocketIO from "socket.io";
 import * as room from "../action/snug";
-import {CONSUME_EVENT} from "../common/events/consume-type";
+import { CONSUME_EVENT } from "../common/events/consume-type";
 
 export const connect = (io: SocketIO.Server) => {
   const snugIo = io.of("/snug");
@@ -8,7 +8,9 @@ export const connect = (io: SocketIO.Server) => {
 
   snugIo.on(CONSUME_EVENT.CONNECTION, (socket: SocketIO.Socket) => {
     const req = socket.request;
-    const { headers: { referer } } = req;
+    const {
+      headers: { referer }
+    } = req;
     console.log(`${socket.id} ${referer} 클라이언트 접속`);
 
     const profileId: string = null;
@@ -18,14 +20,22 @@ export const connect = (io: SocketIO.Server) => {
       console.log(`${socket.id} ${referer} 클라이언트 나감`);
       room.leave(socket, profileId);
     });
+
+    socket.on(CONSUME_EVENT.NEWJOIN, channelId => {
+      console.log("join", channelId);
+      socket.join(channelId);
+      console.log(`${socket.id} ${referer} 클라이언트가 새로운 방 접속`);
+    });
   });
 
   userIo.on(CONSUME_EVENT.CONNECTION, (socket: SocketIO.Socket) => {
     const req = socket.request;
-    const { headers: { referer } } = req;
+    const {
+      headers: { referer }
+    } = req;
     console.log(`${socket.id} ${referer} 클라이언트 접속`);
 
-    socket.on("login", ({userId}) => {
+    socket.on("login", ({ userId }) => {
       socket.join(userId);
       socket.on(CONSUME_EVENT.DISCONNECTION, () => {
         console.log(`${socket.id} ${referer} 클라이언트 나감`);
