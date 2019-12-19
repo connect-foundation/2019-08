52118210dfad03be0b999208bba6a8179150f721
sangwon <junothyme@gmail.com>
2019-12-19 17:26:00 +0900

    feat: register-snug를, 비로그인 상태로 방문했을 때, 에러 페이지로 인도

--- client/src/presentation/pages/register-snug/index.tsx
+++ client/src/presentation/pages/register-snug/index.tsx
@@ -2,14 +2,20 @@ import React from "react";
 import { PageLayout } from "presentation/components/atomic-reusable/page-layout";
 import { RegisterSnugForm } from "./register-snug-form";
 import { ApplicationProptype } from "prop-types/application-type";
+import { ErrorComponent } from "presentation/pages/error";
 
 // 해더에서 초대 수신을 동적으로 확인하기 위해 socket 역시 필요하다.
-export const RegisterSnug: React.FC<ApplicationProptype> = (props) => {
+export const RegisterSnug: React.FC<ApplicationProptype> = props => {
   const { Application } = props;
+  const user = Application.services.authService.getUserInfo();
 
   return (
     <PageLayout Application={Application}>
-      <RegisterSnugForm Application={Application}></RegisterSnugForm>
+      {user.id ? (
+        <RegisterSnugForm Application={Application} />
+      ) : (
+        <ErrorComponent />
+      )}
     </PageLayout>
   );
 };
