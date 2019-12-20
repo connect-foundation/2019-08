5b5649b0ff1839b3d1aaebeecb5379895193178e
Seung Bin . Ko <kosb15@naver.com>
2019-12-20 18:47:11 +0900

    fix: 다중 프로세스 지원하는 pm2 인하여 Socket Room 의 정합성이 맞지 않아 메시지가 오지 않는 이슈
    why:
    - pm2 는 무중단 서비스를 지원하기 위해, 프로그램을 다중 프로세스로 실행한다
    - 이 과정에서 Socket Room 에 참여하는 사용자가 프로세스마다 나뉘어 지게 되어 메시지를 받지 못하는 이슈가 발생
    - pm2 를 하나의 프로세스로 동작시켜 이슈 해결

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,5 @@
 import path from "path";
 import dotenv from "dotenv";
-const config =
-        process.env.NODE_ENV === "production" ? ".env.production" : ".env.local";
-dotenv.config({path: path.join(__dirname, "..", config)});
 import Application from "./app";
 import {Express} from "express";
 import http from "http";
@@ -11,28 +8,34 @@ import {initialize} from "./socket/socket-manager";
 import {initializeMailManger} from "./mail/mail-manager";
 import fs from "fs";
 import HttpHelper from "./utils/http-helper";
+import logger from "./utils/logger";
 
+const config =
+        process.env.NODE_ENV === "production" ? ".env.production" : ".env.local";
+dotenv.config({path: path.join(__dirname, "..", config)});
 const applyHttps = (app: Express): boolean => {
-  if(HttpHelper.isSupportedHttp()) {
-    return false;
+  if(HttpHelper.isSupportedHttps()) {
+    const keyPath = process.env.KEY_DIR;
+    const encoding = "utf8";
+    const key = fs.readFileSync(path.join(__dirname, keyPath, process.env.PRIVATE_KEY), encoding);
+    const cert = fs.readFileSync(path.join(__dirname, keyPath, process.env.CERT_KEY), encoding);
+    const ca = fs.readFileSync(path.join(__dirname, keyPath, process.env.CA_KEY), encoding);
+    const options = {key, cert, ca};
+    const server = https
+            .createServer(options, app)
+            .listen(443, () => initialize(server));
+    return true;
   }
 
-  const keyPath = "../.key";
-  const key = fs.readFileSync(path.join(__dirname, keyPath, process.env.PRIVATE_KEY), "utf8");
-  const cert = fs.readFileSync(path.join(__dirname, keyPath, process.env.CERT_KEY), "utf8");
-  const options = {key, cert};
-  const server = https
-          .createServer(options, app)
-          .listen(443, () => initialize(server));
-  return true;
+  return false;
 };
 
 Application.start().then((app: Express) => {
   const server: http.Server = app.listen(app.get("port"), () => {
-    console.log("listen port", app.get("port"));
+    logger.info("listen port", app.get("port"));
     initializeMailManger();
-    const isNotAppliedHttps = applyHttps(app);
-    if (isNotAppliedHttps) {
+    const isAppliedHttps = applyHttps(app);
+    if (!isAppliedHttps) {
       initialize(server);
     }
   });
