de823390c506fc6161571c9b34d79df421f01b33
Seung Bin . Ko <kosb15@naver.com>
2019-12-20 10:17:48 +0900

    chore: 오브젝트 스토리지 관련 환경변수를 참조 못하는 이슈에 대한 리뷰 반영

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,8 @@
 import path from "path";
 import dotenv from "dotenv";
+const config =
+        process.env.NODE_ENV === "production" ? ".env.production" : ".env.local";
+dotenv.config({path: path.join(__dirname, "..", config)});
 import Application from "./app";
 import {Express} from "express";
 import http from "http";
@@ -8,10 +11,6 @@ import {initialize} from "./socket/socket-manager";
 import {initializeMailManger} from "./mail/mail-manager";
 import fs from "fs";
 import HttpHelper from "./utils/http-helper";
-
-const config =
-        process.env.NODE_ENV === "production" ? ".env.production" : ".env.local";
-dotenv.config({path: path.join(__dirname, "..", config)});
 
 const applyHttps = (app: Express): boolean => {
   if(HttpHelper.isSupportedHttp()) {
